da801308c6b2f57b140ee580c4ed157b61398932
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2020-12-27 16:01:16 -0600

    Update run.sh

--- MacShell-master/run.sh
+++ MacShell-master/run.sh
@@ -2,11 +2,15 @@
 
 openssl req -new -newkey rsa:2048 -nodes -out ca.csr -keyout ca.key -subj "/C=US/ST=CA/L=Redwood City/O=Mac Experts LLC" && openssl x509 -trustout -signkey ca.key -days 365 -req -in ca.csr -out ca.pem
 
+echo Enter the IP/hostname of your MacShell server:
+
+read server
+
 echo Enter listening port for your MacShell server:
 
 read port
 
-python3 generator.py -s 0.0.0.0 -p $port
+python3 generator.py -s $server -p $port
 
 docker build -t macshell-docker .
 
